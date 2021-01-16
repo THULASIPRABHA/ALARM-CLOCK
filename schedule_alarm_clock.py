b6216d80e0ab8abf51c978a4e37cc827f50b33ca
THULASIPRABHA <69714505+THULASIPRABHA@users.noreply.github.com>
2021-01-17 00:08:26 +0530

    Update schedule_alarm_clock.py

--- schedule_alarm_clock.py
+++ schedule_alarm_clock.py
@@ -7,7 +7,7 @@ from plyer import notification
 def day():
     TNOW = datetime.datetime.now().replace(microsecond=0)
     while True:
-        notification.notify( title = "ALARAM TIME", message=" WAKEUP GO TO YOUR WORK" , app_icon="alarm.ico",timeout=10,toast=True) 
+        notification.notify( title = "ALARM TIME", message=" WAKEUP GO TO YOUR WORK" , app_icon="alarm.ico",timeout=10,toast=True) 
         for i in range(5):
             winsound.Beep(random.randint(37,10000), random.randint(750,3000))
         break
@@ -21,4 +21,4 @@ schedule.every(1).hour.do(min)
 schedule.every(1).day.at("01:45").do(day)
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(10)
